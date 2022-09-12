9178dce1f30c0f0ef0769bf5273ed29b100bc790
Nikitrin <nikita.trinoga@yandex.ru>
2022-09-12 21:59:38 +0300

    feat: add JSON parser

--- src/main/java/ru/yandex/trinoga/nikita/enrollment/controller/Controller.java
+++ src/main/java/ru/yandex/trinoga/nikita/enrollment/controller/Controller.java
@@ -1,21 +1,37 @@
 package ru.yandex.trinoga.nikita.enrollment.controller;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.trinoga.nikita.enrollment.components.schemas.SystemItem;
+import ru.yandex.trinoga.nikita.enrollment.components.schemas.SystemItemImport;
+import ru.yandex.trinoga.nikita.enrollment.components.schemas.SystemItemImportRequest;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 @RestController
 public class Controller {
     Gson gson = new Gson();
 
-    @GetMapping("/import")
-    public String addFilesAndFolders(@RequestBody String body) {
+    @PostMapping("/import")
+    public String addItems(@RequestBody String body) {
+        Type type = new TypeToken<ArrayList<SystemItemImportRequest>>(){}.getType();
 
-        SystemItem item = gson.fromJson(body, SystemItem.class);
+        ArrayList<SystemItemImportRequest> items = gson.fromJson(body, type);
 
-        return item.toString();
+        for (SystemItemImportRequest item : items) {
+            System.out.println(item.getUpdateDate());
+            if (item.getItems() != null) {
+                for (SystemItemImport file : item.getItems()) {
+                    System.out.println(file);
+                }
+            }
+        }
+
+        return items.toString();
     }
 }
